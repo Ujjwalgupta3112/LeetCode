0cf873308daac9c1109d0c4d9e077984db85d1fa
Ujjwal Gupta <ujjwalgupta1268@gmail.com>
2026-09-01 09:37:07 +0530

    Time: 133 ms (51.04%) | Memory: 308.8 MB (12.58%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public boolean isPath(ArrayList<Integer>[] graph, int src, int dest, boolean[] visited) {
+        if(src == dest) return true;
+        visited[src] = true;
+        for(int nbr: graph[src]){
+            if(visited[nbr] == false){
+                if(isPath(graph, nbr, dest, visited)) return true;
+            }
+        }
+        return false;
+    }
+    public boolean validPath(int n, int[][] edges, int source, int destination){
+        ArrayList<Integer>[] graph = new ArrayList[n];
+        for(int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+                for(int[] edge : edges) {
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+        }
+                boolean[] visited = new boolean[n];
+        return isPath(graph, source, destination, visited);
+    }
+}
